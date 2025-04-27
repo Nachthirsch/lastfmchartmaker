e8984696796552b7cca1da2674e8802f427a15d6
Nachthirsch <revengecounter202@gmail.com>
2025-04-27 18:41:21 +0700

    Update lastfm.api.js to set a fixed limit of 15 for top artists, albums, and tracks retrieval methods, ensuring consistent data fetching across the application.

--- src/services/lastfm.api.js
+++ src/services/lastfm.api.js
@@ -213,7 +213,7 @@ export const lastfmService = {
           method: "user.gettopartists",
           user: username,
           period, // overall, 7day, 1month, 3month, 6month, 12month
-          limit,
+          limit: 15,
           page,
         },
       });
@@ -230,7 +230,7 @@ export const lastfmService = {
           method: "user.gettopalbums",
           user: username,
           period,
-          limit,
+          limit: 15,
           page,
         },
       });
@@ -247,7 +247,7 @@ export const lastfmService = {
           method: "user.gettoptracks",
           user: username,
           period,
-          limit,
+          limit: 15,
           page,
         },
       });
